38deaea6b0799a13815aca5eb49559690018c3cb
YassarG <yassarghinata26@students.unnes.ac.id>
2023-12-05 16:54:41 +0700

    update tanggal 5 masuk database

--- backend.js
+++ backend.js
@@ -3,6 +3,8 @@ const express = require("express"); //require berfungsi untuk mengambil modul ex
 const app = express();
 const port = 3000; //dijalankan di port 3000 atau bebas
 const fs = require("fs");
+// app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 // const cors = require("cors");
 
 // Server.use(cors());
@@ -22,7 +24,26 @@ app.get("/products", (req, res) => {
   });
 });
 
+app.get("/products/:id", (req, res) => {
+  const { id } = req.params;
+
+  fs.readFile("./products.json", (error, data) => {
+    if (error) res.send("Gagal dalam pembacaan data");
+    const products = JSON.parse(data);
+    const product = products.find((product) => product.id === parseInt(id));
+    if (!product) {
+      res.status(404).send("Product not found");
+    }
+    res.status(200).send(product);
+  });
+});
+
 //bikin get baru sama app.post
+
+app.post("/products", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
 
 app.all("*", (req, res) => {
   res.status(404).send("404 routes not found");
